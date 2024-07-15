72445479ba0cc19d865e0af77c6051b154761337
B00TK1D <stearns.josiah@gmail.com>
2024-07-15 19:36:47 -0400

    Expose prn power over tcp

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@
 mkdir /data
 rm /data/gnss.fifo
 mkfifo /data/gnss.fifo
-gnss-sdr --config-file=/conf/udp.conf &
+gnss-sdr --config-file=/conf/udp.conf 2>&1 | grep PSIG_DUMP | nc -kl 1502 &
 recvpipe
