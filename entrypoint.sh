2a93fd029ec4103828c76c372d86098971872198
B00TK1D <stearns.josiah@gmail.com>
2024-07-16 07:35:37 -0400

    Constrain grep to only match

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,6 @@
 mkdir /data
 rm /data/gnss.fifo
 mkfifo /data/gnss.fifo
-gnss-sdr --config-file=/conf/udp.conf 2>&1 | grep PSIG_DUMP | nc -kl 1502 &
+gnss-sdr --config-file=/conf/udp.conf 2>&1 | grep -oE 'PSIG_DUMP: PRN=\d+ PSig=[0-9\.]+' | nc -kl 1502 &
+# PSIG_DUMP: PRN=14 PSig=474.226
 recvpipe
